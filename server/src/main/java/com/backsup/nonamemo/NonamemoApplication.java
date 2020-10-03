1965c9e06894028b9b5a29d254859c5ea2459a10
JeongWon Liee <khjvvv7@gmail.com>
2020-10-03 22:25:00 +0900

    init - hello nonamemo server

---
+++ server/src/main/java/com/backsup/nonamemo/NonamemoApplication.java
@@ -0,0 +1,13 @@
+package com.backsup.nonamemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NonamemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NonamemoApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/backsup/nonamemo/NonamemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backsup.nonamemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NonamemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
