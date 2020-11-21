3face17c3298c07e71c22507c7211fb441fb14ea
GardenLee <khjvvv7@gmail.com>
2020-11-21 16:28:35 +0900

    feat: set security configuration
    - 기능 개발 완료 까지 모든 request 허용
    - csrf disable
    - session 사용 x

---
+++ server/src/main/java/com/backsup/nonamemo/config/SecurityConfiguration.java
@@ -0,0 +1,27 @@
+package com.backsup.nonamemo.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    /**
+     * csrf disable
+     * session 사용 X
+     * 기능 개발 완료 까지 모든 request 보안 설정 X
+     * */
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http.httpBasic().disable()
+                .csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                    .authorizeRequests()
+                    .anyRequest().permitAll();
+    }
+}
